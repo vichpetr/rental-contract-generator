96395c2b042598a1a7a8ddcb77395d7e0eb18b9a
Petr Vích <petr@petrvich.eu>
2025-12-07 17:06:39 +0100

    add vite-plugin-federation

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'rental_generator',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App.jsx',
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
+  build: {
+    target: 'esnext'
+  }
 })
